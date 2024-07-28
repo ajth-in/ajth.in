6f5907af0208841eba8bbdd53848b5065fe2d324
Ajith Kumar P M <ajithpmuralidharan01@gmail.com>
2024-07-28 09:42:52 +0000

    chore: add hero section

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { CldImage } from "next-cloudinary";
+
 function ArrowIcon() {
   return (
     <svg
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { BlogPosts } from "app/components/posts";
-
+import { HeroSection } from "./components/Hero";
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Hello there.
-      </h1>
-      <p className="mb-4">
+      <HeroSection />
+      <p>
         {`I'm Ajith Kumar P M, but you can call me Ajith. I'm a front-end engineer based on India. Here are a few projects and experiences I've made so far.`}
       </p>
       <hr className="border-.1-dashed" />
