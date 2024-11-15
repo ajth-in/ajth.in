3362f7fac97c756a1d7660f6ee32085ad648b3f3
ajith-k <ajithpmuralidharan01@gmail.com>
2024-11-15 23:19:25 +0530

    chore: add image bento grid

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,95 +1,25 @@
-import Marquee from "@/components/ui/marquee";
+
 import { cx } from "class-variance-authority";
+import Link from "next/link";
 
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
-];
-
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
-
-const ReviewCard = ({
-  img,
-  name,
-  username,
-  body,
-}: {
-  img: string;
-  name: string;
-  username: string;
-  body: string;
-}) => {
+const GalleryPage = () => {
+    const blogbg="https://plus.unsplash.com/premium_photo-1673306778968-5aab577a7365?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmFja2dyb3VuZCUyMGltYWdlfGVufDB8fDB8fHww"
+    const images = [blogbg,blogbg,blogbg,blogbg,blogbg]
+    const containerStyles= ["row-span-3 col-span-1","row-span-1 col-span-1","row-span-2 col-span-1","row-span-2 col-span-1","row-span-2 col-span-1"]
   return (
-    <figure
-      className={cx(
-        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-        // light styles
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-      )}
-    >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
-    </figure>
+     <div className="grid p-4 lg:grid-cols-3 h-100 gap-2 lg:grid-rows-3 bg-dark">
+        {images.map((image,index)=>(<div  className={cx("relative flex  border border-gray-300/20 rounded-lg bg-dark", containerStyles[index])} >
+        <img
+          style={{ objectFit: "cover" }}
+          className="rounded-xl opacity-20 hover:opacity-70 duration-500"
+          src={image}
+        />
+      </div>))}
+      
+      
+    </div>
   );
 };
-export default function GalleryPage() {
-  return (
-    <>
-      <Marquee  className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
-        ))}
-      </Marquee>
-      <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
-        ))}
-      </Marquee>
-    </>
-  );
-}
+
+
+export default GalleryPage
