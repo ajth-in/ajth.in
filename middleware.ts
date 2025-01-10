b6abfe60a89eef80268a90ce2b798e6a05881073
Ajith Kumar P M <ajith@strollby.com>
2025-01-10 22:53:01 +0530

    fix(tinyUrl): made link public route

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { GALLERY, HOME } from "app/constants/routes";
+import { HOME } from "app/constants/routes";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
+  "/link(.*)",
   HOME,
 ]);
 export default clerkMiddleware(async (auth, request) => {
