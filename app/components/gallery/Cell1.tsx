95191f86f78a231baff23da0f3cdb9cdab094afa
Ajith Kumar P M <ajith@strollby.com>
2025-02-15 13:29:52 +0530

    fix: remove all existing files

--- app/components/gallery/Cell1.tsx
+++
@@ -1,3 +0,0 @@
-export const Cell1 = () => {
-  return;
-};
--- app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <section className="flex justify-center items-center min-h-[calc(100dvh-280px)]">
-      <SignIn />
-    </section>
-  );
-}
