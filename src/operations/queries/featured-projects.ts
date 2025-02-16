936f1f6dde8d562417de92992fe7cfe4b08b1de5
Ajith Kumar P M <ajith@strollby.com>
2025-02-16 08:54:52 +0530

    feat: add projects preview section

---
+++ src/operations/queries/featured-projects.ts
@@ -0,0 +1,8 @@
+// import { db } from "~/server/db";
+// import { projectsTable, SelectTable } from "~/server/db/schema";
+
+// export const getFeaturedProjects = async (
+//   limit: number = 5
+// ): Promise<Array<SelectTable>> => {
+//   return db.select().from(projectsTable).limit(limit);
+// };
