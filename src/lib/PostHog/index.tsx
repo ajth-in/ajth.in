b8f211dbedfcfdb7beab2f81f5ce6b56fe3525e3
Ajith Kumar P M <ajith@strollby.com>
2025-06-01 12:13:09 +0530

    fix: conditionally initialize PostHog in production environment

--- src/lib/PostHog/index.tsx
+++ src/lib/PostHog/index.tsx
@@ -2,16 +2,17 @@
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
-import { PropsWithChildren, useEffect } from "react";
+import { type PropsWithChildren, useEffect } from "react";
 import PostHogPageView from "~/app/PostHogPageView";
 
 export function PostHogProvider({ children }: PropsWithChildren) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-    });
+    if (process.env.NODE_ENV === "production")
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      });
   }, []);
 
   return (
