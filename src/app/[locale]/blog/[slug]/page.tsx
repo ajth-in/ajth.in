f6dd241b2ec9eca23e4f2a428aa12dc4d61df43e
Ajith Kumar P M <ajith@strollby.com>
2025-06-02 23:41:12 +0530

    feat: implement BlogContent component for dynamic blog rendering and add Spinner for loading state

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -1,68 +1,28 @@
 import { redirect } from "next/navigation";
-import { Fragment } from "react";
+import { Fragment, Suspense } from "react";
+import Spinner from "~/components/Spinner";
 import { HOME } from "~/constants/routes";
-import { BlogHeader } from "~/modules/blogs/BlogHeader";
-import { MarkdownRenderer } from "~/modules/blogs/MarkdownRender";
+import BlogContent from "~/modules/blogs/BlogContent";
 import Header from "~/modules/home/Header";
 import { type PageProps } from "~/types";
-
-export interface Blog {
-  type_of: string;
-  id: number;
-  title: string;
-  description: string;
-  readable_publish_date: string;
-  slug: string;
-  path: string;
-  url: string;
-  comments_count: number;
-  public_reactions_count: number;
-  collection_id: string;
-  published_timestamp: string;
-  language: string;
-  subforem_id: string;
-  positive_reactions_count: number;
-  cover_image: string;
-  social_image: string;
-  canonical_url: string;
-  created_at: string;
-  edited_at: string;
-  crossposted_at: string;
-  published_at: string;
-  last_comment_at: string;
-  reading_time_minutes: number;
-  tag_list: string;
-  tags: string[];
-  body_html: string;
-  body_markdown: string;
-  user: User;
-}
-
-export interface User {
-  name: string;
-  username: string;
-  twitter_username: string;
-  github_username: string;
-  user_id: number;
-  website_url: string;
-  profile_image: string;
-  profile_image_90: string;
-}
 
 const BlogPage = async (props: PageProps) => {
   const blogId = (await props.params).slug;
   if (!blogId) redirect(HOME);
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_DEV_TO_API}/articles/${blogId}?username=ajith-in`,
-  );
-  const data = (await response.json()) as Blog;
 
   return (
     <Fragment>
       <Header className="max-w-4xl" />
       <main className="max-w-4xl mx-auto py-2">
-        <BlogHeader post={data} />
-        <MarkdownRenderer content={data.body_markdown} />
+        <Suspense
+          fallback={
+            <div className="w-full flex justify-center items-center min-h-[600px]">
+              <Spinner />
+            </div>
+          }
+        >
+          <BlogContent blogId={blogId} />
+        </Suspense>
       </main>
     </Fragment>
   );
---
+++ src/modules/blogs/BlogContent/index.tsx
@@ -0,0 +1,63 @@
+import { Fragment } from "react";
+import { BlogHeader } from "../BlogHeader";
+import { MarkdownRenderer } from "../MarkdownRender";
+
+export interface Blog {
+  type_of: string;
+  id: number;
+  title: string;
+  description: string;
+  readable_publish_date: string;
+  slug: string;
+  path: string;
+  url: string;
+  comments_count: number;
+  public_reactions_count: number;
+  collection_id: string;
+  published_timestamp: string;
+  language: string;
+  subforem_id: string;
+  positive_reactions_count: number;
+  cover_image: string;
+  social_image: string;
+  canonical_url: string;
+  created_at: string;
+  edited_at: string;
+  crossposted_at: string;
+  published_at: string;
+  last_comment_at: string;
+  reading_time_minutes: number;
+  tag_list: string;
+  tags: string[];
+  body_html: string;
+  body_markdown: string;
+  user: User;
+}
+
+export interface User {
+  name: string;
+  username: string;
+  twitter_username: string;
+  github_username: string;
+  user_id: number;
+  website_url: string;
+  profile_image: string;
+  profile_image_90: string;
+}
+type BlogContentProps = { blogId: string };
+const BlogContent = async (props: BlogContentProps) => {
+  const { blogId } = props;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_DEV_TO_API}/articles/${blogId}?username=ajith-in`,
+  );
+  const data = (await response.json()) as Blog;
+
+  return (
+    <Fragment>
+      <BlogHeader post={data} />
+      <MarkdownRenderer content={data.body_markdown} />
+    </Fragment>
+  );
+};
+
+export default BlogContent;
