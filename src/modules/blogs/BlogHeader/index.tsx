8032f566b12d54bfd6ab17f4ef386c73ba9f07ae
ajth-in <ajithpmuralidharan01@gmail.com>
2025-07-02 00:01:31 +0530

    fix: update Blog import path in BlogHeader component

--- src/modules/blogs/BlogHeader/index.tsx
+++ src/modules/blogs/BlogHeader/index.tsx
@@ -7,7 +7,7 @@ import {
 } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Badge } from "~/components/ui/badge";
-import { type Blog } from "../BlogContent";
+import { type Blog } from "~/types";
 
 interface BlogHeaderProps {
   post: Blog;
