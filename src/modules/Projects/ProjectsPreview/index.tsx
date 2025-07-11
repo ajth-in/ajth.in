e75950f017bbb7675e12822dfe3d2b46be72f971
ajth-in <ajithpmuralidharan01@gmail.com>
2025-07-11 20:03:07 +0530

    fix: update GitHub token environment variable in fetch functions

--- src/modules/Projects/ProjectsPreview/index.tsx
+++ src/modules/Projects/ProjectsPreview/index.tsx
@@ -1,22 +1,12 @@
-"use client";
-
 import AllProjectsLink from "../AllProjectsLink";
-import { useQuery } from "@tanstack/react-query";
 import pinnedCommentsQuery, {
   type RepositoryFragment,
 } from "~/operations/queries/get-pinned-projects";
 import ProjectListItem from "./ProjectListItem";
 import { type FragmentOf } from "gql.tada";
-import ProjectsPreviewSkeleton from "./Skeleton";
 
-const ProjectPreview = () => {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["ajth-in"],
-    queryFn: pinnedCommentsQuery,
-  });
-  if (isLoading) return <ProjectsPreviewSkeleton />;
-  if (isError) throw new Error("Invalid response");
-
+const ProjectPreview = async () => {
+  const data = await pinnedCommentsQuery();
   return (
     <div className="flex flex-col gap-2 py-4">
       {data?.user?.pinnedItems.nodes?.map((item, index) => (
--- src/operations/queries/get-pinned-projects.ts
+++ src/operations/queries/get-pinned-projects.ts
@@ -1,5 +1,6 @@
 import { graphql } from "gql.tada";
 import { request } from "graphql-request";
+import { unstable_cache as cache } from "next/cache";
 export const RepositoryFragment = graphql(`
   fragment RepositoryFrag on Repository {
     name
@@ -34,28 +35,32 @@ const query = graphql(
   [RepositoryFragment],
 );
 
-const fetchPinnedProjects = async () => {
-  try {
-    const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+const fetchPinnedProjects = cache(
+  async () => {
+    try {
+      const token = process.env.GITHUB_TOKEN;
 
-    if (!token) {
-      throw new Error("GitHub token is required");
+      if (!token) {
+        throw new Error("GitHub token is required");
+      }
+
+      const result = await request(
+        process.env.NEXT_PUBLIC_GITHUB_API!,
+        query,
+        {},
+        {
+          Authorization: `Bearer ${token}`,
+        },
+      );
+
+      return result;
+    } catch (error) {
+      console.error("GraphQL query error:", error);
+      throw error;
     }
-
-    const result = await request(
-      process.env.NEXT_PUBLIC_GITHUB_API!,
-      query,
-      {},
-      {
-        Authorization: `Bearer ${token}`,
-      },
-    );
-
-    return result;
-  } catch (error) {
-    console.error("GraphQL query error:", error);
-    throw error;
-  }
-};
+  },
+  ["pinned-projects"],
+  { revalidate: 5 * 60 * 60 },
+);
 
 export default fetchPinnedProjects;
