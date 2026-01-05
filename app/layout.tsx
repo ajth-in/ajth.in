11b4feaa35c3f7c6467979aa8a64b93049ba1705
ajth-in <ajithpmuralidharan01@gmail.com>
2026-01-05 21:21:35 +0530

    feat: remove outdated Vim article; add new article on React interview practices; update global styles for improved accessibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        "text-black bg-white dark:text-white dark:bg-black",
+        "text-black bg-white dark:text-white dark:bg-[#1c1b1b]",
         GeistSans.variable,
         GeistMono.variable
       )}
