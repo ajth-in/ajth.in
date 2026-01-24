81774a278bd2d36d0bdd3f75df45d84e9ea857d3
ajth-in <ajithpmuralidharan01@gmail.com>
2026-01-24 22:15:34 +0530

    feat: add past midnight blog

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -19,6 +19,7 @@ function Avatar({
       {...props}
     />
   )
+   
 }
 
 function AvatarImage({
@@ -31,6 +32,7 @@ function AvatarImage({
       className={cn("aspect-square size-full", className)}
       {...props}
     />
+    
   )
 }
 
@@ -50,4 +52,5 @@ function AvatarFallback({
   )
 }
 
-export { Avatar, AvatarImage, AvatarFallback }
+export { Avatar, AvatarFallback, AvatarImage }
+
